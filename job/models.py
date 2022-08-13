7bb43f18b2f179db5f1b04ec121cee23076e75c9
Nazeeer <mohamed55nazeer55@gmail.com>
2022-08-13 15:09:11 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+
+JOB_TYPE=(
+    ('Full Time','Full Time'),
+    ('part Time','part Time'),
+)
+class Job(models.Model):
+    title=models.CharField(max_length=100) 
+    jab_type=models.CharField(max_length=15,choices=JOB_TYPE)
+    description=models.TextField(max_length=1000)
+    publish_at=models.DateTimeField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=1)
+    def __str__(self) :
+        return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'job',
+    'home',
+    'blog',
+    'contact',
+    'accounts',
 ]
 
 MIDDLEWARE = [
